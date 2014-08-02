7cd606171a7b8cbb9ba7ca5e4166237b5b889522
005vinay <005vinay@gmail.com>
2014-08-02 13:08:27 -0700

    this is step-05 and step-06, step-05 json file is added so we can get dynamic product list using $http method step-06 we have added some images for the product.

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,12 +1,8 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller("PhoneListCtrl", function($scope) {
-	$scope.phones =  [
-	{'name': 'Nexus S',
-	'snippet': 'Fast just got faster with Nexus S.'},
-	{'name': 'Motorola XOOM? with Wi-Fi',
-	'snippet': 'The Next, Next Generation tablet.'},
-	{'name': 'Relience',
-	'snippet': 'karalo duniya muththi me.'}
-	];
-});
+phonecatApp.controller("PhoneListCtrl", ['$scope', '$http', function ($scope, $http) {
+    $http.get('phones/phones.json').success(function (data) {
+        $scope.phones = data;
+    });
+    $scope.orderProp = "age";
+}]);
