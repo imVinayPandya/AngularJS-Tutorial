c761a0e0e728e97a213e02a866dfa7c0721155fc
005vinay <005vinay@gmail.com>
2014-08-02 22:08:53 -0700

    this is step-10 event listener is added. dynamic image appears at the place of  main image.

--- app/js/controller.js
+++ app/js/controller.js
@@ -3,6 +3,7 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 phonecatControllers.controller("PhoneListCtrl", ['$scope', '$http', function ($scope, $http) {
     $http.get('phones/phones.json').success(function (data) {
         $scope.phones = data;
+
     });
     $scope.orderProp = "age";
 }]);
@@ -12,5 +13,9 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$h
     $scope.id = $routeParams.phoneId + '.json';
     $http.get('phones/' + $routeParams.phoneId + '.json').success(function (data) {
         $scope.phone = data;
+        $scope.mainImageUrl = data.images[0];
     });
+    $scope.setImage = function (imageUrl) {
+        $scope.mainImageUrl = imageUrl;
+    }
 }]);
